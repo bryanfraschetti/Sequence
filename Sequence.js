e54d7d5a52e67c6ddf942001cbc8160debec1b16
Bryan Fraschetti <bryan.fraschetti@gmail.com>
2022-02-16 22:32:03 -0500

    Commit2

--- Sequence.js
+++ Sequence.js
@@ -10,12 +10,15 @@ function onPageLoad(){}
 function requestAuthorization(){
     client_id = document.getElementById("clientId").value
     client_secret = document.getElementById("clientSecret").value
+    localStorage.setItem("client_id", client_id)
+    localStorage.setItem("client_secret", client_secret)
 
     let url = AUTHORIZE
     url += "?client_id=" + client_id
     url += "&response_type=code"
     url += "&redirect_uri=" + encodeURI(redirect_uri)
     url += "&show_dialog=true"
-    url += "&scope=user-read-currently-playing playlist-modify-private playlist-modify-public"
+    url += "&scope=user-read-currently-playing%20playlist-modify-private%20playlist-modify-public"
+    console.log(url)
     window.location.href = url
 }
