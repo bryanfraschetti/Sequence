cd94f341c098707e55a2d8078c1620a11f55a68e
Bryan Fraschetti <bryan.fraschetti@gmail.com>
2024-08-05 14:26:46 -0400

    [Fix]: Error handling for jwt verify failure

--- server/routes/tokenHandlers/accessToken.js
+++ server/routes/tokenHandlers/accessToken.js
@@ -11,66 +11,72 @@ router.use(bodyParser.json());
 const entryPoint = "/";
 
 router.post("/", (req, res) => {
-  if (req.session.tokens) {
-    // New or continuing session -> overwrite client
-    res.json({
-      access_token: req.session.tokens.access_token,
-      expires: req.session.tokens.expires,
-      refresh_token: req.session.tokens.refresh_token,
-      JWT: req.session.JWT,
-      userId: req.session.userId,
-    });
-  } else if (
-    !req.session.tokens &&
-    req.body.refresh_token &&
-    req.body.access_token &&
-    req.body.expires &&
-    req.body.JWT &&
-    jwt.verify(req.body.JWT, jwtSecret)
-  ) {
-    // Session ended but user had a previous session
-    // Generate fresh tokens
-
-    const clientTokens = {
-      access_token: sanitizeInput(req.body.access_token),
-      expires: sanitizeInput(req.body.expires),
-      refresh_token: sanitizeInput(req.body.refresh_token),
-      JWT: req.body.JWT,
-    };
-
-    fetch("http://nginx/api/RefreshToken", {
-      // Send current state to Sequence
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        access_token: clientTokens.access_token,
-        refresh_token: clientTokens.refresh_token,
-        expires: clientTokens.expires,
-        JWT: clientTokens.JWT,
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then((response) => {
-        res.json(response);
-      })
-      .catch((error) => {
-        console.error(error);
-
-        res.json({
-          redirect_uri: entryPoint,
-        }); // Send redirect
+  try {
+    if (req.session.tokens) {
+      // New or continuing session -> overwrite client
+      res.json({
+        access_token: req.session.tokens.access_token,
+        expires: req.session.tokens.expires,
+        refresh_token: req.session.tokens.refresh_token,
+        JWT: req.session.JWT,
+        userId: req.session.userId,
       });
-  } else {
+    } else if (
+      !req.session.tokens &&
+      req.body.refresh_token &&
+      req.body.access_token &&
+      req.body.expires &&
+      req.body.JWT &&
+      jwt.verify(req.body.JWT, jwtSecret)
+    ) {
+      // Session ended but user had a previous session
+      // Generate fresh tokens
+
+      const clientTokens = {
+        access_token: sanitizeInput(req.body.access_token),
+        expires: sanitizeInput(req.body.expires),
+        refresh_token: sanitizeInput(req.body.refresh_token),
+        JWT: req.body.JWT,
+      };
+
+      fetch("http://nginx/api/RefreshToken", {
+        // Send current state to Sequence
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          access_token: clientTokens.access_token,
+          refresh_token: clientTokens.refresh_token,
+          expires: clientTokens.expires,
+          JWT: clientTokens.JWT,
+        }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.json();
+        })
+        .then((response) => {
+          res.json(response);
+        })
+        .catch((error) => {
+          console.error(error);
+
+          res.json({
+            redirect_uri: entryPoint,
+          }); // Send redirect
+        });
+    } else {
+      res.json({
+        redirect_uri: entryPoint,
+      }); // Send redirect
+    }
+  } catch {
     res.json({
       redirect_uri: entryPoint,
-    }); // Send redirect
+    });
   }
 });
 
--- server/routes/tokenHandlers/refreshToken.js
+++ server/routes/tokenHandlers/refreshToken.js
@@ -18,69 +18,75 @@ const spotifyTokenUrl = "https://accounts.spotify.com/api/token";
 router.post("/", (req, res) => {
   const refresh_token = req.body.refresh_token;
   const JWT = req.body.JWT;
-  if (!refresh_token || !jwt.verify(JWT, jwtSecret)) {
-    // Missing credentials
-    res.json({
-      redirect_uri: "/401",
-    }); // Send redirect
-  } else {
-    fetch(
-      spotifyTokenUrl +
-        "?grant_type=refresh_token&refresh_token=" +
-        sanitizeInput(refresh_token),
-      {
-        headers: {
-          Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
-          "content-type": "application/x-www-form-urlencoded",
-        },
-        method: "POST",
-      }
-    )
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Response not OK");
+  try {
+    if (!refresh_token || !jwt.verify(JWT, jwtSecret)) {
+      // Missing credentials
+      res.json({
+        redirect_uri: "/401",
+      }); // Send redirect
+    } else {
+      fetch(
+        spotifyTokenUrl +
+          "?grant_type=refresh_token&refresh_token=" +
+          sanitizeInput(refresh_token),
+        {
+          headers: {
+            Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+            "content-type": "application/x-www-form-urlencoded",
+          },
+          method: "POST",
         }
-      })
-      .then((jsonData) => {
-        const date = new Date();
-        const expiryTime = date.getTime() + 1000 * jsonData.expires_in;
-        return {
-          access_token: jsonData.access_token,
-          expires: expiryTime,
-          refresh_token: refresh_token,
-        };
-      })
-      .then((tokens) => {
-        // Store tokens in session and send to user
-        req.session.tokens = tokens;
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Response not OK");
+          }
+        })
+        .then((jsonData) => {
+          const date = new Date();
+          const expiryTime = date.getTime() + 1000 * jsonData.expires_in;
+          return {
+            access_token: jsonData.access_token,
+            expires: expiryTime,
+            refresh_token: refresh_token,
+          };
+        })
+        .then((tokens) => {
+          // Store tokens in session and send to user
+          req.session.tokens = tokens;
 
-        const verifiedJwt = jwt.verify(JWT, jwtSecret);
-        const userId = verifiedJwt.userId;
+          const verifiedJwt = jwt.verify(JWT, jwtSecret);
+          const userId = verifiedJwt.userId;
 
-        const payload = {
-          refreshToken: tokens.refresh_token,
-          userId: userId,
-        };
+          const payload = {
+            refreshToken: tokens.refresh_token,
+            userId: userId,
+          };
 
-        const JWTNew = jwt.sign(payload, jwtSecret);
+          const JWTNew = jwt.sign(payload, jwtSecret);
 
-        req.session.JWT = JWTNew;
+          req.session.JWT = JWTNew;
 
-        res.json({
-          access_token: req.session.tokens.access_token,
-          expires: req.session.tokens.expires,
-          refresh_token: req.session.tokens.refresh_token,
-          JWT: JWTNew,
+          res.json({
+            access_token: req.session.tokens.access_token,
+            expires: req.session.tokens.expires,
+            refresh_token: req.session.tokens.refresh_token,
+            JWT: JWTNew,
+          });
+        })
+        .catch((error) => {
+          // console.error(error)
+          res.json({
+            redirect_uri: entryPoint,
+          }); // Send redirect
         });
-      })
-      .catch((error) => {
-        // console.error(error)
-        res.json({
-          redirect_uri: entryPoint,
-        }); // Send redirect
-      });
+    }
+  } catch {
+    res.json({
+      redirect_uri: entryPoint,
+    }); // Send redirect
   }
 });
 
