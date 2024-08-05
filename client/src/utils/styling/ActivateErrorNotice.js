7d8085585d3112b0d5fc6c9b3d3fe3d6e96d4759
Bryan Fraschetti <bryan.fraschetti@gmail.com>
2024-08-05 16:40:43 -0400

    [Dev]: Activate Error Notice logs error

--- client/src/utils/styling/ActivateErrorNotice.js
+++ client/src/utils/styling/ActivateErrorNotice.js
@@ -1,9 +1,9 @@
 export const ActivateErrorNotice = (ERR) => {
+  console.log(ERR);
   const FetchAnimation = document.getElementById("loading-container");
   if (FetchAnimation) {
     FetchAnimation.style.display = "none"; // Disable loading animation
   }
   const ErrorNotice = document.getElementById("error-notice");
   ErrorNotice.style.display = "block"; // Enable error status
-  //   console.error(ERR);
 };
