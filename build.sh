ceb554436571de76af20219ab768175bfd69262d
Bryan Fraschetti <bryan.fraschetti@gmail.com>
2024-08-30 12:51:30 -0400

    [Feature]: Added more granular control over build.sh (argument specific image rebuilding)

--- build.sh
+++ build.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-react_container=$(docker container list -a | grep "react" | awk '{print $1}' | xargs)
-if [ -n "$react_container" ]; then
-    docker stop $react_container
-fi
-
-api_container=$(docker container list -a | grep "api" | awk '{print $1}' | xargs)
-if [ -n "$api_container" ]; then
-    docker stop $api_container
-fi
-
-react_image=$(docker image list | grep "react" | awk '{print $3}' | xargs)
-if [ -n "$react_image" ]; then
-    docker rmi -f $react_image
-fi
-
-api_image=$(docker image list | grep "api" | awk '{print $3}' | xargs)
-if [ -n "$api_image" ]; then
-    docker rmi -f $api_image
-fi
-
-redis_image=$(docker image list | grep "redis" | awk '{print $3}' | xargs)
-if [ -n "$redis_image" ]; then
-    docker rmi -f $redis_image
-fi
-
-nginx_image=$(docker image list | grep "nginx" | awk '{print $3}' | xargs)
-if [ -n "$nginx_image" ]; then
-    docker rmi -f $nginx_image
-fi
-
-
-cd ./client
-# npm install # If dependencies not yet installed
-npm run build
+docker compose down
 
 redis-cli shutdown
 
-# Go back to the root directory and start Docker Compose
-cd ..
+delete_container() {
+    container_name=$1
+    container_id=$(docker container list -a | grep "$container_name" | awk '{print $1}' | xargs)
+    if [ -n "$container_id" ]; then
+        docker stop $container_id
+        docker rm $container_id
+    fi
+}
+
+delete_image() {
+    image_name=$1
+    image_id=$(docker image list | grep "$image_name" | awk '{print $3}' | xargs)
+    if [ -n "$image_id" ]; then
+        docker rmi -f $image_id
+    fi
+}
+
+compile_react(){
+    # This is not automated as part of the dockerfile because it is a time consuming step
+    # It should only be executed if changes were made to the React app rather than every instance start
+    cd ./client
+    # npm install # If dependencies not yet installed
+    npm run build
+    # Go back to the root directory and start Docker Compose
+    cd ..
+}
+
+if [ -z "$1" ]; then
+    # No arguments, rebuild everything
+    docker rmi -f $(docker images -aq)
+
+    compile_react
+
+else
+    # Iterate over each argument provided
+    for name in "$@"; do
+        delete_container "$name"
+        delete_image "$name"
+        
+        if [ "$name" = "react" ]; then
+            compile_react
+        fi
+    done
+fi
+
 docker compose up --detach
